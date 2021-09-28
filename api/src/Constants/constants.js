d8ce1e8523c984b4d4703ae6a040358881466995
Matias Costilla <matiascostilla96@gmail.com>
2021-09-28 18:07:18 -0300

    Back Tuneado

--- api/src/Constants/constants.js
+++
@@ -1,9 +0,0 @@
-const URL = 'https://pokeapi.co/api/v2/';
-const POKEMON = "pokemon";
-const TYPE = "type"
-
-module.exports = {
-    URL,
-    POKEMON,
-    TYPE
-};
--- api/src/routes/type.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const { getAllTypes} = require('../metodos/type');
-const router = Router();
-
-
-router.get('/', getAllTypes);
-
-
-module.exports = router;
