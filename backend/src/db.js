225c0509b769c7a25952cc115525bf1e6f794468
Matias Costilla <matiascostilla96@gmail.com>
2021-09-28 09:47:04 -0300

    Models

--- backend/src/db.js
+++ backend/src/db.js
@@ -6,7 +6,7 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/poke`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/tukiteck`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
@@ -30,7 +30,7 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Pokemon, Type } = sequelize.models;
+const { Categorias, Eventos, Subcategorias, Tickets, Usuarios } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
