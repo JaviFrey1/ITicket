39b91d42ebed032b2757607b9cdc4680932a038f
Francisco Palacios <palacios.francisco@outlook.com>
2021-09-29 10:34:55 -0300

    CSS a cards de eventos: add boton, change detalles

--- client/src/App.js
+++ client/src/App.js
@@ -2,29 +2,25 @@ import "./App.css";
 
 import React from "react";
 import { Route, BrowserRouter } from "react-router-dom";
-import Event from './components/Event/Event'
+import Event from "./components/Event/Event";
 import WishList from "./components/WishList/WishList";
 import EventDetail from "./components/EventDetail/EventDetail";
 import Nav from "./components/Nav/Nav";
 import Home from "./components/Home/Home";
 import AddEvent from "./components/AddEvent/AddEvent";
+import Events from "./components/Events/Events";
 
 function App() {
   return (
     <BrowserRouter>
       <React.Fragment>
         <Route
-          path={[
-            "/home",
-            "/events/:id",
-            "/wishList",
-            "/addEvent",
-            
-          ]}
+          path={["/home", "/events/:id", "/wishList", "/addEvent"]}
           component={Nav}
         />
         <Route path="/home" component={Home} />
-        <Route path='/event' component={Event}/>
+        <Route path="/event" component={Event} />
+        <Route path="/events" component={Events} />
         <Route path="/events/:id" component={EventDetail} />
         <Route path="/wishList" component={WishList} />
         <Route path="/addEvent" component={AddEvent} />
--- client/src/components/Event/Event.jsx
+++ client/src/components/Event/Event.jsx
@@ -2,19 +2,23 @@ import React from "react";
 import styles from "./Event.module.css";
 
 export default function Event({ name, img, fecha, cat }) {
-
   // const dispatch = useDispatch();
   // const wish = useSelector((state) => state.wishEvents);
   // <Link  to={`/recipes/${props.id}`}>
   // !img? img ='Not Found': img;
   return (
-    
     <div className={styles.clas}>
-      <div className={styles.contenedor} style={{backgroundImage: `url('${img}')`}}>
+      <div
+        className={styles.contenedor}
+        style={{ backgroundImage: `url('${img}')` }}
+      >
         <div className={styles.hijo}>
           <div className={styles.nombre}>{name}</div>
           <div className={styles.cat}>{cat}</div>
           <div className={styles.fecha}>{fecha}</div>
+        </div>
+        <div className={styles.contBtn}>
+          <div className={styles.boton}>Mas info</div>
         </div>
       </div>
     </div>
