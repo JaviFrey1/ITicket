19463fc406c5fe265ce5c54216875ebf34bf674d
Francisco Palacios <palacios.francisco@outlook.com>
2021-10-02 14:38:12 -0300

    centrado de cards en home y detalle de numero de paginado

--- client/src/components/Events/Events.jsx
+++ client/src/components/Events/Events.jsx
@@ -1,23 +1,20 @@
 import Event from "../Event/Event";
 import s from "./Events.module.css";
 
-
-
-export default function Events({events}) {
+export default function Events({ events }) {
   return (
     <div className={s.divRey}>
-
-      {events.length>0? events.map(event=>{return(
-
-        <div key={event.id}>
-
-          <Event
-          event={event}
-         
-        />
-        </div>
-      )
-      }) : (<h1>No hay eventos disponibles</h1>)}
+      {events.length > 0 ? (
+        events.map((event) => {
+          return (
+            <div className={s.contEvents} key={event.id}>
+              <Event event={event} />
+            </div>
+          );
+        })
+      ) : (
+        <h1>No hay eventos disponibles</h1>
+      )}
     </div>
   );
 }
