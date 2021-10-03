4051b08702446bd7286259a61d3704c3648ff11d
randikun <guadafantini@gmail.com>
2021-10-03 20:37:06 -0300

    refactorizacion del get

--- api/src/controllers/events.js
+++ api/src/controllers/events.js
@@ -1,66 +1,64 @@
 const { Events, Categories, SubCategories } = require("../db");
-const { Op } = require("sequelize");
 
+
+async function finder() {
+  const dataBase = await Events.findAll({
+    include: [
+      {
+        model: Categories,
+        through: {
+          attributes: [],
+        },
+      },
+
+      {
+        model: SubCategories,
+        through: {
+          attributes: [],
+        },
+      },
+    ],
+  });
+  if (dataBase.length > 0) {
+    const eventDb = dataBase.map((result) => {
+      return {
+        name: result.name,
+        id: result.id,
+        category: result.categories.map((cat) => cat.name),
+        subCategories: result.subCategories.map((subCat) => subCat.genre),
+        artist: result.artist,
+        place: result.place,
+        address: result.address,
+        location: result.location,
+        province: result.province,
+        image: result.image,
+        price: result.price,
+        availableTickets: result.availableTickets,
+        date: result.date,
+        time: result.time,
+      };
+    });
+    return eventDb
+  }
+}
 
 async function getAllEvents(req, res) {
   let name = req.query.name;
 
-  // let page = req.query.page; //Nuevo
-  // let allEvents = []; //Nuevo          
-  // const eventsPerPage = 4;//Nuevo
-  // page = page? page : 1; //Nuevo
-
-
-
   if (name) {
     const searcheado = name.toLowerCase();
     try {
-      const dataBase = await Events.findAll({
-        include: [
-          {
-            model: Categories,
-            through: {
-              attributes: [],
-            },
-          },
-
-          {
-            model: SubCategories,
-            through: {
-              attributes: [],
-            },
-          },
-        ],
-      });
-      if (dataBase.length > 0) {
-        const eventDb = dataBase.map((result) => {
-          return {
-            name: result.name,
-            id: result.id,
-            category: result.categories.map((cat) => cat.name),
-            subCategories: result.subCategories.map((subCat) => subCat.genre),
-            artist: result.artist,
-            place: result.place,
-            address: result.address,
-            location: result.location,
-            province: result.province,
-            image: result.image,
-            price: result.price,
-            availableTickets: result.availableTickets,
-            date: result.date,
-            time: result.time,
-          };
-        });
-      
+      const eventDb = await finder()
+      if (eventDb.length > 0) {
         const filtered = eventDb.filter(
           (event) =>
             event.artist.toLowerCase().includes(searcheado) ||
             event.name.toLowerCase().includes(searcheado)
         );
-        // let result = allEvents.slice((eventsPerPage * (page - 1)), (eventsPerPage * (page - 1)) + eventsPerPage);// Nuevo
 
         return res.json(filtered);
-      } else return res.send("Evento Inexistente");
+
+      } else return res.send([]);
     } catch (error) {
       return res
         .status(400)
@@ -68,51 +66,10 @@ async function getAllEvents(req, res) {
     }
   } else {
     try {
-      // const {page, size} = req.query; //Nuevo (forma Mati --> QUERY)
 
-      const dataBase = await Events.findAll({
-        // limit:size, //Nuevo (Forma Mati --> QUERY)
-        // offset: page * size, //Nuevo (Forma Mati --> QUERY)
-
-        include: [
-          {
-            model: Categories,
-            through: {
-              attributes: [],
-            },
-          },
-
-          {
-            model: SubCategories,
-            through: {
-              attributes: [],
-            },
-          },
-        ],
-      });
-      if (dataBase.length > 0) {
-        const eventDb = dataBase.map((result) => {
-          return {
-            name: result.name,
-            id: result.id,
-            category: result.categories.map((cat) => cat.name),
-            subCategories: result.subCategories.map((subCat) => subCat.genre),
-            artist: result.artist,
-            place: result.place,
-            address: result.address,
-            location: result.location,
-            province: result.province,
-            image: result.image,
-            price: result.price,
-            availableTickets: result.availableTickets,
-            date: result.date,
-            time: result.time,
-          };
-        });
-        // let result = eventDb.slice((eventsPerPage * (page - 1)), (eventsPerPage * (page - 1)) + eventsPerPage);// Nuevo
-
-        res.json(eventDb); // lo cambie por eventDB
-      } else return res.send([]);
+      const dataBase = await finder()
+      if (dataBase.length > 0) return res.send(dataBase)
+      else return res.send([]);
     } catch (error) {
       return res
         .status(400)
