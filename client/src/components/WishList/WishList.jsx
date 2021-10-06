eab5a782b629c0bb2aceb0b7f7887659c45625ef
Javier Frey <javierfreyy@hotmail.com>
2021-10-05 22:12:39 -0300

    fix de la whishlist NO TERMINADO

--- client/src/components/WishList/WishList.jsx
+++ client/src/components/WishList/WishList.jsx
@@ -1,20 +1,24 @@
 import React from "react";
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 import Event from "../Event/Event";
 import s from "./wishlist.module.css";
+import { loadState} from "../../localStorage";
 
 export function WishList() {
-  const wishEvents = useSelector((state) => state.wishEvents);
+  // const wishEvents = useSelector((state) => state.wishEvents);
+  const favorites = loadState();
+  console.log('Soy favorites',favorites)
   return (
     <div className={s.page}>
       <div className={s.container}>
         <h2 className={s.titulo}> Estos eventos te han interesado!</h2>
-        {wishEvents ? (
+        {favorites ? (
           <div className={s.cards}>
-            {wishEvents.map((we) => {
+            {favorites.slice(1).map(fav => {
+              JSON.parse(fav);
               return (
-                <div key={we.id} className={s.event}>
-                  <Event event={we}/>
+                <div key={fav.id} className={s.event}>
+                  <Event event={fav}/>
                 </div>
               );
             })}
