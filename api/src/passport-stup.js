8440efbc993d991b0747acd8c0c3f7e3c7009a04
Luca Di Menna <saodimenna@gmail.com>
2021-10-06 20:12:52 -0300

    login de google terminado

--- api/src/passport-stup.js
+++ api/src/passport-stup.js
@@ -1,6 +1,6 @@
-const { Users } = require('./db.js')
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const { Users } = require("./db.js");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 // passport.serializeUser(function (user, done) {
 //   done(null, user.id);
@@ -12,12 +12,11 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 //   });
 // });
 
-
 // passport.use(new GoogleStrategy({
 //     clientID: '606271626860-n6ttqfol40fnplghb6lr832bknsmp5u6.apps.googleusercontent.com',
 //     clientSecret: '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',
 //     callbackURL: "http://localhost:3001/google/callback"
-//   }, 
+//   },
 //   function(accessToken, refreshToken, profile, done) {
 //     console.log('USUARIOOOOOOOOO',profile.id)
 //     Users.findOrCreate({ googleId: profile.id }, function (err, user) {
@@ -26,55 +25,48 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 //   }
 // ));
 
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID:
+        "606271626860-n6ttqfol40fnplghb6lr832bknsmp5u6.apps.googleusercontent.com",
+      clientSecret: "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",
+      callbackURL: "http://localhost:3001/google/callback",
+      passReqToCallback: true,
+    },
+    async (req, accessToken, refreshToken, profile, cb) => {
+      const defaultUser = {
+        fullName: `${profile.name.givenName} ${profile.name.familyName}`,
+        email: profile.emails[0].value,
+        picture: profile.photos[0].value,
+        googleId: profile.id,
+      };
 
-passport.use(new GoogleStrategy({
-  clientID: '606271626860-n6ttqfol40fnplghb6lr832bknsmp5u6.apps.googleusercontent.com',
-  clientSecret: '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',
-  callbackURL: "http://localhost:3001/google/callback",
-  passReqToCallback: true
-}, async (req, accessToken, refreshToken, profile, cb) => {
-  const defaultUser = {
-    fullName: `${profile.name.givenName} ${profile.name.familyName}`,
-    email: profile.emails[0].value,
-    picture: profile.photos[0].value,
-    googleId: profile.id
-
-  }
-
-  const user = await Users.findOrCreate({ where: { googleId: profile.id, fullName: `${profile.name.givenName} ${profile.name.familyName}` }, defaultUser: defaultUser }).catch((err) => {
-
-    console.log("Error al logearse xdnt", err)
-    cb(err, null)
-  }
-
+      const user = await Users.findOrCreate({
+        where: { googleId: profile.id },
+        defaults: defaultUser,
+      }).catch((err) => {
+        console.log("Error al logearse xdnt", err);
+        cb(err, null);
+      });
+      if (user && user[0]) {
+        return cb(null, user && user[0]);
+      }
+    }
   )
-  if (user && user[0]) {
-      return cb(null, user && user[0])
-  }
-
-
-}
-)
-
 );
 
-
-passport.serializeUser((user, cb)=>{
-
+passport.serializeUser((user, cb) => {
   console.log("serializing user", user);
   cb(null, user.id);
-
 });
 
-passport.deserializeUser( async (id, cb) =>{
-  const user = await Users.findOne({where: {id}}).catch((err)=>{
+passport.deserializeUser(async (id, cb) => {
+  const user = await Users.findOne({ where: { id } }).catch((err) => {
     console.log("Error xdnt", err);
     cb(err, null);
-  })
+  });
 
   console.log("Deserialized", user);
-  if(user) cb(null, user);
-})
-
-
-
+  if (user) cb(null, user);
+});
