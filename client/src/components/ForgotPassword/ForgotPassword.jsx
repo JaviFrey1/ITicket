1bff474636882a5a7924bfbafc6c9cbf5d167194
randikun <guadafantini@gmail.com>
2021-10-19 01:23:28 -0300

    toggle password visibility en forgot

--- client/src/components/ForgotPassword/ForgotPassword.jsx
+++ client/src/components/ForgotPassword/ForgotPassword.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react"
 import { useParams } from "react-router";
 import { useHistory } from "react-router-dom";
-import { useDispatch} from "react-redux";
+import { useDispatch } from "react-redux";
 import confirmForgot from "../../actions/confirmForgot";
 
 import s from './ForgotPassword.module.css'
@@ -9,6 +9,8 @@ import { BsFillEyeFill, BsFillEyeSlashFill } from 'react-icons/bs'
 
 
 export default function ForgotPassword() {
+    const [passwordShown, setPasswordShown] = useState(false);
+    const [confirmShown, setConfirmShown] = useState(false)
 
 
     const [pass, setPass] = useState('');
@@ -19,12 +21,12 @@ export default function ForgotPassword() {
     const history = useHistory();
     const { id } = useParams();
 
-    const input = document.querySelector('#password');
-    function toggle() {
-        const type = input.getAttribute('type') === 'password' ? 'text' : 'password';
-        input.setAttribute('type', type);
-
-    }
+    const togglePasswordVisiblity = () => {
+        setPasswordShown(passwordShown ? false : true);
+    };
+    const toggleConfirmVisiblity = () => {
+        setConfirmShown(confirmShown ? false : true);
+    };
     const validate = () => {
         if (pass.length > 0) {
             if (pass !== confirm) {
@@ -42,11 +44,10 @@ export default function ForgotPassword() {
         setConfirm(e.target.value);
     }
     const submit = () => {
-        console.log(input.getAttribute("type"))
-        // validate();
+        validate();
         // setPass('');
         // setConfirm('');
-        dispatch(confirmForgot(id,confirm));
+        dispatch(confirmForgot(id, confirm));
         history.push('/home')
     }
 
@@ -57,17 +58,18 @@ export default function ForgotPassword() {
                 <div className={s.contInputs}>
                     <input name='pass1' value={pass}
                         autoComplete="off" id='password'
-                        type="password" className={s.input}
+                        type={passwordShown ? "text" : "password"} className={s.input}
                         placeholder="Nueva contraseña"
                         onChange={(e) => handleInput1Change(e)}
-                    /> {input.type === 'password' ? <BsFillEyeFill onClick={() => toggle()} />
-                        : <BsFillEyeSlashFill onClick={() => toggle()} />}
+                    /> {passwordShown === false ? <BsFillEyeFill className={s.icon} onClick={() => togglePasswordVisiblity()} />
+                        : <BsFillEyeSlashFill className={s.icon} onClick={() => togglePasswordVisiblity()} />}
                     <input name='pass2' value={confirm}
-                        type="password" className={s.input}
+                        type={confirmShown ? "text" : "password"} className={s.input}
                         autoComplete="off"
                         placeholder="Reingresa la contraseña"
                         onChange={(e) => handleInput2Change(e)}
-                    />
+                    />{confirmShown === false ? <BsFillEyeFill className={s.icon} onClick={() => toggleConfirmVisiblity()} />
+                        : <BsFillEyeSlashFill className={s.icon} onClick={() => toggleConfirmVisiblity()} />}
                     <div>
                         {errors.length > 0 ? <h5 className={s.error}>{errors}</h5> : <div>HOLA</div>}
                     </div>
