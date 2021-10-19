44f42a940e1d6761306d9a3bd5b6d56911195638
Javi <javierfreyy@hotmail.com>
2021-10-19 18:36:57 -0300

    deploy

--- api/src/app.js
+++ api/src/app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const routes = require('./routes/index');
+const routes = require('./routes/index.js');
 const session = require('express-session');
 const passport = require('passport');
 const cors = require('cors')
 
 
-require('./db');
+require('./db.js');
 
 const server = express();
 
@@ -16,16 +16,16 @@ const server = express();
 server.name = 'API';
 
 
+server.use(cors({ origin: "*", credentials: true }))
 server.use(express.urlencoded({ extended: true, limit: '50mb' }));
 server.use(express.json({ limit: '50mb' }));
-server.use(cors({ origin: "*", credentials: true }))
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => { // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Origin', "*");// update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  // res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
 
