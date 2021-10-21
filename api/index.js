32baad50f27b9a6a23907b04d66855637493d96f
Javi <javierfreyy@hotmail.com>
2021-10-21 11:12:42 -0300

    deploy

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ const { conn } = require("./src/db.js");
 // Syncing all the models at once.
 conn.sync({ force : false}).then(() => {
 
-  server.listen(process.env.PORT, () => {
+  server.listen(process.env.PORT || 5432, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
