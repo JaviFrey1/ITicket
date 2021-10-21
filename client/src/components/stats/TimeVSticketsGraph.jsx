12c7363ad0395bb465848db19177d9988f1c2779
randikun <guadafantini@gmail.com>
2021-10-21 14:24:44 -0300

    grafico no repitee fechas(salvo fecha de creacion)

--- client/src/components/stats/TimeVSticketsGraph.jsx
+++ client/src/components/stats/TimeVSticketsGraph.jsx
@@ -13,19 +13,32 @@ export default function TimeVSticketsGraph({ artist }) {
     const tags = []
     const data = []
 
-    stats.map(obj => { 
+    stats.map(obj => {
         tags.push(obj.date);
         data.push(obj.availableTickets)
         return stats
     })
-      tags.sort(function (a, b) {
+    tags.sort(function (a, b) {
         if (a > b) return 1;
         else if (a < b) return -1;
         return 0;
-      })
+    })
+    let indices = []
+    indices.push(0)
+    for (let i = 1; i < tags.length; i++) {
+        if (tags[i] !== tags[i + 1]) indices.push(i)
+    }
+
+    const correctTags = []
+    const correctData = []
+    indices.map(i => {
+        correctTags.push(tags[i])
+        correctData.push(data[i])
+        return indices
+    })
     const ticketsData = {
         label: artist + ' Tickets disp. en funcion del tiempo',
-        data: data,
+        data: correctData,
         backgroundColor: 'rgba(107, 44, 167, 0.705)',
         borderColor: 'rgb(255, 204, 0)',
         hoverBackgroundColor: 'rgb(107, 42, 167)',
@@ -37,14 +50,14 @@ export default function TimeVSticketsGraph({ artist }) {
 
     useEffect(() => {
         dispatch(timeVStickets(artist));
-    }, [dispatch,artist]);
+    }, [dispatch, artist]);
 
     useEffect(() => {
 
         const timeVStickets = new Chart($stats, {
             type: 'line',
             data: {
-                labels: tags,
+                labels: correctTags,
                 datasets: [
                     ticketsData,
                 ]
@@ -65,7 +78,7 @@ export default function TimeVSticketsGraph({ artist }) {
             }
         });
         return () => { timeVStickets.destroy() }
-    }, [stats,$stats,tags,ticketsData]);
+    }, [stats, $stats, correctTags, ticketsData]);
 
     return (
 
