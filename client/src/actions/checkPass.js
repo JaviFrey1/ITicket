20a5da1a5c3d482972d59e14fc09371f4c0a6c35
Javi <javierfreyy@hotmail.com>
2021-10-21 00:31:09 -0300

    deploy final

--- client/src/actions/checkPass.js
+++ client/src/actions/checkPass.js
@@ -5,7 +5,7 @@ export default function checkPass(id, pass) {
     return async function (dispatch) {
         try {
             
-            const response = await axios.get(`http://localhost:3001/checkPass/${id}?password=${pass}`);
+            const response = await axios.get(`/checkPass/${id}?password=${pass}`);
             
             dispatch({type: CHECK_PASS, payload: response.data})
         } catch (error) {
